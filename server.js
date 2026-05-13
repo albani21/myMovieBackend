bfe7e316d4639f34ad89f3b301293b9464a03ec7
alban misimi <alban.misimi.20@gmail.com>
2026-05-13 17:27:43 +0200

    changed the sync function

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api", authRouter);
 app.listen(PORT, async () => {
 	try {
 		await sequelize.authenticate();
-		await sequelize.sync();
+		await sequelize.sync({force: true});
 		console.log("the DATA BASE is RUNNING...")
 		console.log("the server is listening in PORT ", PORT)
 	} catch (e) {
